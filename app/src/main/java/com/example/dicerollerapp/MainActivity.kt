90dc9629025107d759085598eb8af0ffa3a56205
VARUN KUMAR <varunjha089@gmail.com>
2021-06-22 21:30:04 +0530

    Image will change dynamically now using when statement, with a toast message.

--- app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.example.dicerollerapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Finding the button in layout and saving the reference in variable 'rollButton'
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            // Displaying a toast message in starting.
+            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
+            toast.show()
+
+            // Updating the textView
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        // This is just for sample
+        //diceImage.setImageResource(R.drawable.dice_2)
+
+        // Changing image based on response from "Dice class"
+        when (diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            6 -> diceImage.setImageResource(R.drawable.dice_6)
+        }
     }
 }
 
