3227475bf23608596817f697b63b793fa1bd20e6
VARUN KUMAR <varunjha089@gmail.com>
2021-06-22 21:14:50 +0530

    Class Dice created to return a random number.

--- app/src/main/java/com/example/dicerollerapp/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.dicerollerapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+}
+
+class Dice(private val numSide: Int) {
+
+    // A roll() method to generate a random number between 1 and 6.
+    fun roll(): Int {
+        return (1..numSide).random()
     }
 }
